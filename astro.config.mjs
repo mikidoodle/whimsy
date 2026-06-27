e9f08a10958699f796411fe9c36ecb54dbdd3478
katiikati <166103545+the-kat-kat@users.noreply.github.com>
2026-06-26 21:30:45 -0400

    add astro site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'whimsy.hackclub.com',
   vite: {
     plugins: [tailwindcss()]
   }
